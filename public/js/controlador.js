c0275a85343858d37a069799823f6369eff403f8
josuemr <41358360+jrm96@users.noreply.github.com>
2018-12-09 02:54:26 -0600

    Modificado paso a paso y cambios varios

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -4,26 +4,29 @@ var dirInstruccion = 0; //indica la posición de la instrucción a ejecutar
 var instruccion = 0; //Instruccion a ejecutar
 var valor = 0; //valor que acompaña la instruccion
 var flag = true;
+var paso = 0;
 
 
 $('document').ready(function () {
-    
+
     incializarMemoria();
 
 });
 
 $("#correr").click(function () {
     cargarPrograma();
-     ejecutarPrograma();
-    iniciarVariables();
-    
+    ejecutarPrograma();
+
 });
+
 $("#paso").click(function () {
-    cargarPrograma();
+    if (paso == 0)
+        cargarPrograma();
     ejecutarPasoPaso();
-   
-     
+    paso++;
+
 });
+
 $("#limpiar").click(function () {
     $("#codigo").val("");
     location.reload(true);
@@ -38,10 +41,11 @@ function iniciarVariables() {
     dirInstruccion = 0; //indica la posición de la instrucción a ejecutar
     instruccion = 0; //Instruccion a ejecutar
     valor = 0; //valor que acompaña la instruccion
+    paso = 0;
 }
 
 function incializarMemoria() {
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 1000; i++) {
         memoria.push(0);
     }
 }
@@ -54,19 +58,32 @@ function cargarPrograma() {
         }
         else {
             memoria[i] = parseInt(contenido[i]);
-            $('#memoria').val( $('#memoria').val()+'00'+i+'-->'+contenido[i]+ '\n');
         }
     }
     obtenerInstruccion();
+    mostrarEstado()
 }
 
-
+function mostrarEstado() {
+    $('#memoria').val("");
+    var line = '0';
+    for (i = 0; i < memoria.length; i++) {
+        if (i < 10)
+            line = '00';
+        else if (i < 100)
+            line = '0';
+        else
+            line = ''
+        $('#memoria').val($('#memoria').val() + line + i + '-->' + memoria[i] + '\n');
+    }
+    $('#linea').val(dirInstruccion);
+}
 
 
 function ejecutarPrograma() {
     contador = 0;
     while (instruccion != 43 && contador < 1001) {
-        
+
         if (instruccion == 10) {
             lee();
         }
@@ -105,82 +122,104 @@ function ejecutarPrograma() {
             break;
         }
         else {
-            alert("Ocurrió un error en la línea " + dirInstruccion);
-            break;
+            $("#log").css('color', 'rgb(201, 0, 0)')
+            console.log('\n' + '------------------------------------------------------------------------' + '\n' + "Ocurrió un error en la dirección de memoria: " + dirInstruccion + '\n' + 'Reinicie el programa');
+            $("#correr").prop("disabled", true);
+            $("#paso").prop("disabled", true);
+            var psconsole = $('#log');
+            if (psconsole.length)
+                psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());        
+            return;
         }
+        var psconsole = $('#log');
+        if (psconsole.length)
+            psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
         contador++;
         obtenerInstruccion();
+        mostrarEstado()
     }
-    
+    $("#correr").prop("disabled", true);
+    $("#paso").prop("disabled", true);
+    iniciarVariables();
     console.log('Programa Finalizado');
 }
 
 
 function ejecutarPasoPaso() {
+    if (instruccion == 10) {
+        lee();
+    }
+    else if (instruccion == 11) {
+        escribirPantalla();
+    }
+    else if (instruccion == 20) {
+        cargarAcumulador();
+    }
+    else if (instruccion == 21) {
+        almacenardeAcumulador();
+    }
+    else if (instruccion == 30) {
+        sumar();
+    }
+    else if (instruccion == 31) {
+        restar();
+    }
+    else if (instruccion == 32) {
+        dividir();
+    }
+    else if (instruccion == 33) {
+        multiplicar();
+    }
+    else if (instruccion == 40) {
+        bifurcar();
+    }
+    else if (instruccion == 41) {
+        bifurcarNeg();
+    }
+    else if (instruccion == 42) {
+        bifurcarCero();
+    }
+    else if (instruccion == 43) {
+        $("#correr").prop("disabled", true);
+        $("#paso").prop("disabled", true);
+        console.log('Programa finalizado');
+        iniciarVariables();
+        return false;
+    }
+    else {
+        $("#log").css('color', 'rgb(201, 0, 0)')
+        console.log('\n' + '------------------------------------------------------------------------' + '\n' + "Ocurrió un error en la dirección de memoria: " + dirInstruccion + '\n' + 'Reinicie el programa');
+        $("#correr").prop("disabled", true);
+        $("#paso").prop("disabled", true);
+        var psconsole = $('#log');
+        if (psconsole.length)
+            psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());    
+        return false;
+    }
+    var psconsole = $('#log');
+    if (psconsole.length)
+        psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+    obtenerInstruccion();
+    mostrarEstado();
 
-        
-        if (instruccion == 10) {
-            lee();
-        }
-        else if (instruccion == 11) {
-            escribirPantalla();
-        }
-        else if (instruccion == 20) {
-            cargarAcumulador();
-        }
-        else if (instruccion == 21) {
-            almacenardeAcumulador();
-        }
-        else if (instruccion == 30) {
-            sumar();
-        }
-        else if (instruccion == 31) {
-            restar();
-        }
-        else if (instruccion == 32) {
-            dividir();
-        }
-        else if (instruccion == 33) {
-            multiplicar();
-        }
-        else if (instruccion == 40) {
-            bifurcar();
-        }
-        else if (instruccion == 41) {
-            bifurcarNeg();
-        }
-        else if (instruccion == 42) {
-            bifurcarCero();
-        } 
-        else if (instruccion == 43) {
-            console.log('Finalizado');
-            return false;
-        }
-        else {
-            alert("Ocurrió un error en la línea " + dirInstruccion);
-            return false;
-        } 
-       
-        obtenerInstruccion();
-        setTimeout(function(){ ejecutarPasoPaso(); }, 3000);
-   
 }
 
 
 
-        
+
 function obtenerInstruccion() {
 
     instruccion = parseInt(memoria[dirInstruccion].toString().substring(0, 2));
     valor = parseInt(memoria[dirInstruccion].toString().substring(2, 6));
-      $('#instruccion').val(instruccion);
-    
+    $('#instruccion').val(instruccion);
+
 }
 
 //Instruccion 10
 function lee() {
     let ingresado = prompt("Dato");
-    console.log('Introduzca el valor  '+ingresado);
+    if (ingresado == null) ingresado = 0;
+    console.log('Se ingreso el valor  ' + ingresado + '\n' + '------------------------------------------------------------------------');
     memoria[parseInt(valor)] = parseInt(ingresado);
     dirInstruccion += 1;
 
@@ -203,40 +242,40 @@ function cargarAcumulador() {
 
 //Instruccion 21
 function almacenardeAcumulador() {
-    console.log('Almecena acumulador en --> '+valor)
+    console.log('Almacena acumulador en --> ' + valor + '\n' + '------------------------------------------------------------------------')
     memoria[valor] = acumulador;
     dirInstruccion += 1;
 }
 
 //Instruccion 30
 function sumar() {
-     console.log('Suma --> '+acumulador+'+'+memoria[valor]);
+    console.log('Suma --> ' + acumulador + '+' + memoria[valor] + '\n' + '------------------------------------------------------------------------');
     acumulador = acumulador + memoria[valor];
-     $('#acumulador').val(acumulador);
+    $('#acumulador').val(acumulador);
     dirInstruccion += 1;
 }
 
 //Instruccion 31
 function restar() {
-     console.log('Resta --> '+acumulador+'-'+memoria[valor]);
+    console.log('Resta --> ' + acumulador + '-' + memoria[valor] + '\n' + '------------------------------------------------------------------------');
     acumulador = acumulador - memoria[valor];
-     $('#acumulador').val(acumulador);
+    $('#acumulador').val(acumulador);
     dirInstruccion += 1;
 }
 
 //Instruccion 32
 function dividir() {
-     console.log('Divide --> '+acumulador+'/'+memoria[valor]);
+    console.log('Divide --> ' + memoria[valor] + '/' +acumulador + '\n' + '------------------------------------------------------------------------');
     acumulador = memoria[valor] / acumulador;
-     $('#acumulador').val(acumulador);
+    $('#acumulador').val(acumulador);
     dirInstruccion += 1;
 }
 
 //Instruccion 33
 function multiplicar() {
-     console.log('Multiplica --> '+acumulador+'*'+memoria[valor]);
+    console.log('Multiplica --> ' + acumulador + '*' + memoria[valor] + '\n' + '------------------------------------------------------------------------');
     acumulador = memoria[valor] * acumulador;
-     $('#acumulador').val(acumulador);
+    $('#acumulador').val(acumulador);
     dirInstruccion += 1;
 }
 
